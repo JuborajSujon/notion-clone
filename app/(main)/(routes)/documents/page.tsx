0fbe9fd09ac94e76d4768468246c17ca41045842
Juboraj Sujon <mdsujon72@gmail.com>
2025-02-07 13:48:38 +0600

    Image update

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,5 +1,37 @@
-import React from "react";
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
 
 export default function DocumentsPage() {
-  return <div>This is a protected page.</div>;
+  const { user } = useUser();
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        height={300}
+        width={300}
+        alt="Empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        height={300}
+        width={300}
+        alt="Empty"
+        className="dark:block hidden"
+      />
+
+      <h2 className="text-lg font-medium">
+        Welcome, {`${user?.lastName}`}&apos;s Jotion
+      </h2>
+
+      <Button>
+        <PlusCircle className="w-6 h-6 mr-2" />
+        Create a note
+      </Button>
+    </div>
+  );
 }
