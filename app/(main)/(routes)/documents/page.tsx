a2736d19c4934a6c271cdcb543b176cebab2d618
Juboraj Sujon <mdsujon72@gmail.com>
2025-02-09 10:43:29 +0600

    Schema and API setup create and get function check

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -4,9 +4,26 @@ import Image from "next/image";
 import { useUser } from "@clerk/clerk-react";
 import { Button } from "@/components/ui/button";
 import { PlusCircle } from "lucide-react";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 
 export default function DocumentsPage() {
   const { user } = useUser();
+
+  const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = create({
+      title: "Untitled",
+    });
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created successfully",
+      error: "Failed to create a new note",
+    });
+  };
+
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
       <Image
@@ -28,7 +45,7 @@ export default function DocumentsPage() {
         Welcome, {`${user?.lastName}`}&apos;s Jotion
       </h2>
 
-      <Button>
+      <Button onClick={onCreate}>
         <PlusCircle className="w-6 h-6 mr-2" />
         Create a note
       </Button>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "sonner";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -47,6 +48,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
             storageKey="jotion-theme">
+            <Toaster position="bottom-center" />
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
